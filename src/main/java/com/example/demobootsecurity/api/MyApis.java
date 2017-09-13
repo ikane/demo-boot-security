86ba9a205439ccdf2ba0f3ac19dc32755c37e3b6
ikane <ibrahima.kane@verifone.com>
2017-09-13 15:39:02 +0200

    add encrypting

--- src/main/java/com/example/demobootsecurity/api/MyApis.java
+++ src/main/java/com/example/demobootsecurity/api/MyApis.java
@@ -1,9 +1,15 @@
 package com.example.demobootsecurity.api;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demobootsecurity.util.StringEncryptor;
 
 @RestController
 public class MyApis {
@@ -11,5 +17,32 @@ public class MyApis {
 	@GetMapping(path="connectedUser")
 	public Object getConnectedUser(Principal principal) {
 		return principal;
+	}
+	
+	@GetMapping(value = "urlCrypted/{login}")
+	public String getEncryptedUrl(@PathVariable String login) {
+		
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("login",login);
+		map.put("topSecret","Waffles are tasty");
+		
+		try {		
+			String[] encrypted = StringEncryptor.encryptObject(map);	
+            // url may differ.. based upon project initial context
+			String urlString = "http://localhost:8080/view?d="+encrypted[0]+"&v="+encrypted[1];
+			System.out.println(urlString);
+			return urlString;
+		}catch(Exception e) {
+			//logger.debug("Unable to encrypt view id: "+id, e);
+		}	
+		return "";
+	}
+	
+	@GetMapping(path = "view")
+	public String getDencryptedUrlView(@RequestParam String d, @RequestParam String v) throws Exception {
+		// The data goes out urlEncoded.. but comes in decoded (No Need to url decode on the request)		
+		Object obj = StringEncryptor.decryptObject(d, v);
+		System.out.println("Object Decrypted: "+obj.toString());
+		return obj.toString();
 	}
 }
--- src/main/java/com/example/demobootsecurity/controllers/MyController.java
+++ src/main/java/com/example/demobootsecurity/controllers/MyController.java
@@ -2,6 +2,10 @@ package com.example.demobootsecurity.controllers;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demobootsecurity.util.StringEncryptor;
 
 @Controller
 public class MyController {
@@ -29,14 +35,58 @@ public class MyController {
 	    httpHeaders.add("mmsUser", "admin");
 	    return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
 	}
-	
+	/*
 	@GetMapping("/external_app3")
-	public ResponseEntity<Object> redirectToExternalUrl2(HttpServletRequest request) throws URISyntaxException {
-		String redirectUrl = request.getScheme() + "://localhost:8181";
-	    URI google = new URI(redirectUrl);
-	    HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setLocation(google);
-	    httpHeaders.add("mmsUser", "admin");
-	    return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+	public ResponseEntity<Object> redirectToExternalUrl2(HttpServletRequest request) {
+		
+		try {
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHH");
+			
+			Map<String,String> map = new HashMap<String,String>();
+			map.put("login", "admin");
+			map.put("connectionTime", sdf.format(new Date()));
+			
+			String[] encrypted = StringEncryptor.encryptObject(map);
+			
+			String redirectUrl = request.getScheme() + "://localhost:8181?d="+encrypted[0]+"&v="+encrypted[1];
+			
+			URI google = new URI(redirectUrl);
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.setLocation(google);
+			
+			
+			return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	    
+	}
+	*/
+	@GetMapping("/external_app3")
+	public String redirectToExternalUrl2(HttpServletRequest request) {
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHH");
+			
+			Map<String,String> map = new HashMap<String,String>();
+			map.put("login", "'admin'");
+			map.put("connectionTime", "'" + sdf.format(new Date()) + "'");
+			
+			String[] encrypted = StringEncryptor.encryptObject(map);
+			
+			String redirectUrl = request.getScheme() + "://localhost:8181?d="+encrypted[0]+"&v="+encrypted[1];
+			
+			return "redirect:" + redirectUrl;	  
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+			return "";
+		}
+	    
 	}
 }
